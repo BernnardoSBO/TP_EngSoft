de151d460601d96597bd7f6cce0da0a8c8f24997
luiza <luiza13lv@gmail.com>
2024-05-14 19:02:48 -0300

    teste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h1>teste</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
